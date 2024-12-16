ed4f779c7c8dcb6293767b7f832ddbec3be86273
Karan Roushan <karan.122roushan@gmail.com>
2024-12-16 15:54:13 +0530

    minor adjustments and comments

--- DS_ALGO/Arrays/long_re_char_repl.cpp
+++ DS_ALGO/Arrays/long_re_char_repl.cpp
@@ -9,9 +9,9 @@ int longest_repeat_char_replacement(std::string& s, int k){
     
     std::unordered_map <char, int> charcount;
     int most_frequent_character_count = INT_MIN;
-    int no_of_replacement = k;
-    int characters_to_be_replaced = 0;
-    int window_size = 0;
+    // int no_of_replacement = k;
+    // int characters_to_be_replaced = 0;
+    // int window_size = 0;
     int max_window_size = 0;
     int left = 0 , right = 0; 
     
@@ -25,7 +25,7 @@ int longest_repeat_char_replacement(std::string& s, int k){
 
         
         //now calculate if the window formed is a valid window. nad calculate the max window size at this point
-        while(right - left + 1 - most_frequent_character_count > no_of_replacement){//means not a valid window.
+        while(right - left + 1 - most_frequent_character_count > k){//means not a valid window.
             //means we have to shrink the window till the window becomes valid.
             int left_char = s[left];
             charcount[left_char]--;            
@@ -44,7 +44,7 @@ int longest_repeat_char_replacement(std::string& s, int k){
 int main(){
     std::string s = "CBBBAC";
     int k = 1;
-    std::cout<<longest_repeat_char_replacement(s, k);
+    std::cout<<"maximum window size: "<<longest_repeat_char_replacement(s, k);
 }
 
 
@@ -61,9 +61,9 @@ int main(){
     
 //     std::unordered_map <char, int> charcount;
 //     int most_frequent_character_count = INT_MIN;
-//     int no_of_replacement = k;
-//     int characters_to_be_replaced = 0;
-//     int window_size = 0;
+//     //int no_of_replacement = k;
+//     //int characters_to_be_replaced = 0;
+//     //int window_size = 0;
 //     int max_window_size = 0;
 //     int left = 0 , right = 0; 
     
@@ -72,24 +72,23 @@ int main(){
 //         char current_character = s[right];
 //         charcount[current_character]++;//keep the current character in the frequency map.
         
-//         //calculate the count of most frequent character.
+        //calculate the count of most frequent character.
 //         most_frequent_character_count = std::max(charcount[current_character], most_frequent_character_count);
 
         
-//         //now calculate if the window formed is a valid window. nad calculate the max window size at this point
-//         window_size = right - left + 1;
-//         max_window_size = std::max(max_window_size, window_size);
-//         characters_to_be_replaced = window_size - most_frequent_character_count;
+        //now calculate if the window formed is a valid window. nad calculate the max window size at this point
+//       //window_size = right - left + 1;not needed.
+//         //max_window_size = std::max(max_window_size, window_size);should not be calculated here .
+//         //characters_to_be_replaced = window_size - most_frequent_character_count;not needed.
 //         while(characters_to_be_replaced > no_of_replacement){//means not a valid window.
-//             //means we have to shrink the window till the window becomes valid.
+            //means we have to shrink the window till the window becomes valid.
 //             int left_char = s[left];
 //             charcount[left_char]--;
-//             if(charcount[left_char] == most_frequent_character_count){
-//                 most_frequent_character_count--;
-//             }
+//            // if(charcount[left_char] == most_frequent_character_count){    //condition not needed.
+//              //   most_frequent_character_count--; should not be decreased manually 
+//             //}
 //             left++;
 //         }
-//         //now check if the window is valid;
 //         right++;
 //     }
 
