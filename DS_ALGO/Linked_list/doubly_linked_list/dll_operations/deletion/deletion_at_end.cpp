7883888c81e3950408a3a5110506d2b872077b3f
Karan Roushan <karan.122roushan@gmail.com>
2024-12-23 21:17:22 +0530

    linked list deletion at the end

---
+++ DS_ALGO/Linked_list/doubly_linked_list/dll_operations/deletion/deletion_at_end.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <memory>
+
+
+struct Node{
+    int data;
+    std::unique_ptr<Node> next;
+    Node* previous;
+    Node(const int& value):data(value), next(nullptr), previous(nullptr){}
+};
+
+class d_linked_list{
+    std::unique_ptr<Node> head;
+    Node* t_ptr;
+    public:
+        d_linked_list():head(nullptr), t_ptr(nullptr){};
+        void insert_at_end(const int& value){
+            auto newnode = std::make_unique<Node> (value);
+            if(!head){
+                head = std::move(newnode);
+                t_ptr = head.get();
+                return;
+            }
+            newnode->previous = t_ptr;
+            t_ptr->next = std::move(newnode);
+            t_ptr = t_ptr->next.get();
+        };
+        void delete_at_end(){
+
+            if(!head){
+                std::cout<<"list empty.\n";
+                return;
+            }
+
+            if(!t_ptr->previous){
+                std::cout<<"last element: "<< t_ptr->data<<" is deleted. list emptied. \n";
+                head = nullptr;
+                t_ptr = nullptr;
+                return;
+            }
+            std::cout<<"tail: "<<t_ptr->data<<" is deleted. \n";
+            t_ptr->previous->next = nullptr;
+            t_ptr = t_ptr->previous;
+            std::cout<<t_ptr->data<<" is new tail. \n";
+        };
+        void print(){
+            if(!head){
+                std::cout<<"[].\n";
+                return;
+            }
+            Node* current = head.get();
+            std::cout<<"head->";
+            while (current){
+                std::cout<<current->data;
+                current =current->next.get();
+                if(current){
+                    std::cout<<"<->";
+                }
+            }
+                std::cout<<"<-tail. \n";
+        };
+        ~d_linked_list(){};
+};
+
+int main(){
+    d_linked_list list;
+    list.insert_at_end(12);
+    list.insert_at_end(19);
+    list.insert_at_end(13);
+    list.insert_at_end(11);
+    list.print();
+    std::cout<<"\n";
+    list.delete_at_end();
+    list.print();
+    std::cout<<"\n";
+    list.delete_at_end();
+    list.print();
+    std::cout<<"\n";
+    list.delete_at_end();
+    list.print();
+    std::cout<<"\n";
+    list.delete_at_end();
+    list.print();
+    std::cout<<"\n";
+    list.delete_at_end();
+    list.print();
+    std::cout<<"\n";
+    return 0;
+}
+
